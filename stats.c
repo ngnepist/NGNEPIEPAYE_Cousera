98b98cff9fd556215fa8e6cdfccfb13f55f50c6d
ngnepist <stephane.ngnepiepaye@ensta-bretagne.org>
2021-04-16 23:43:59 +0200

    coding function of statics.c finish

--- stats.c
+++ stats.c
@@ -39,44 +39,83 @@ void main() {
 
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
-
+  printf("Unsorted array\n");
+  print_array(test, SIZE);
+  print_statistics(test, SIZE);
 }
 
 /* Add other Implementation File Code Here */
 void print_statistics(unsigned char* arrayData, unsigned int arraySize){
-
+    sort_array(arrayData, arraySize);
+    printf("Statistics\n");
+    unsigned char maxValue = find_maximum(arrayData, arraySize);
+    unsigned char minValue = find_minimum(arrayData, arraySize);
+    unsigned char medianValue = find_median(arrayData, arraySize);
+    unsigned char meanValue = find_mean(arrayData, arraySize);
+    printf("maximun value = %u \n",(unsigned int) maxValue);
+    printf("minimun value = %u \n",(unsigned int) minValue);
+    printf("median value = %u \n",(unsigned int) medianValue);
+    printf("mean value = %u \n\n",(unsigned int) meanValue);
+    printf("sorted array \n");
+    print_array(arrayData,arraySize);
 
 }
 
 
 void print_array(unsigned char* arrayData, unsigned int arraySize){
-
+    int i = 0;
+    for (i=0 ; i<arraySize ; i++){
+        printf("element %d : %u \n",i,(unsigned int)(arrayData[i]));
+    }
 }
 
 
 unsigned char find_median(unsigned char* arrayData, unsigned int arraySize){
-
+    sort_array(arrayData, arraySize);
+    if (arraySize%2 == 0)
+        return arrayData[(unsigned int)(arraySize/2)];
+    else
+        return (unsigned char)((arrayData[(unsigned int)((arraySize-1)/2)]+arrayData[(unsigned int)((arraySize+1)/2)])/2);
 }
 
 
 unsigned char find_mean(unsigned char* arrayData, unsigned int arraySize){
-
+    int  i=0;
+    unsigned char sum=0;
+    for (i=0 ; i<arraySize ; i++)
+    {
+              sum += arrayData[i];
+    }
+     return (unsigned char)(sum/arraySize);
 }
 
 
 unsigned char find_maximum(unsigned char* arrayData, unsigned int arraySize){
-
+    sort_array(arrayData, arraySize);
+    return arrayData[0];
 }
 
 
 unsigned char find_minimum(unsigned char* arrayData, unsigned int arraySize){
-
+    sort_array(arrayData,arraySize);
+    return arrayData[arraySize-1];
 }
 
 
 void sort_array(unsigned char* arrayData, unsigned int arraySize){
 
-
+    unsigned int i=0,j=0;
+    unsigned char c=0;
+    /* bubble sorting*/
+    for(j=1;j<=arraySize;j++){ // To do the operation ArraySize Time
+        for(i=0;i<arraySize-1;i++){
+            if ( arrayData[i] < arrayData[i+1] ) {
+                    c = arrayData[i];
+                    arrayData[i] = arrayData[i+1];
+                    arrayData[i+1] = c;
+            }
+        }
+    }
 }
 
 
